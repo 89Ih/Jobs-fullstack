b172cad9928d48e21011ea703e843d27324a3712
89Ih <ehabhamza94@gmail.com>
2024-08-26 09:03:51 +0200

    styl.1.3

--- jobs-Frontend/src/pages/Jobs.tsx
+++ jobs-Frontend/src/pages/Jobs.tsx
@@ -7,7 +7,6 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import { JobDetails } from "../components/Parts/JobDetails";
 import CardSkeleton from "../components/Parts/CardSkeleton";
 const Jobs = () => {
-
   const matches = useMediaQuery("(max-width:700px)");
   const inputRef = useRef<HTMLInputElement>(null);
   const [openUp, setOpenUp] = useState<boolean>(false);
@@ -30,7 +29,7 @@ const Jobs = () => {
     if (tJob === "" || undefined) {
       return setQueries(items);
     }
-    return setQueries(queries)
+    return setQueries(queries);
   }
   const multiQuery = useCallback(() => {
     setOpenUp(false);
@@ -51,26 +50,27 @@ const Jobs = () => {
     if (matches) {
       return navigate(`/Job-details/${ID}`);
     }
-    return fetchJobDetails(ID)
+    return fetchJobDetails(ID);
   }
 
   useEffect(() => {
     if (matches) {
-      setOpenUp(false)
+      setOpenUp(false);
     }
     restService.fetchJobs().then((res) => {
-      setItems(res)
-      setQueries(res)
+      setItems(res);
+      setQueries(res);
     });
   }, [filtered, matches]);
   return (
     <>
       <section className="_sub-header" />
-      <section className="mt-10 ">
-        <form className={`flex ${matches && 'flex-wrap justify-between '} gap-3 w-full _searchContainer`}>
-          <div className={!matches ? "w-2/4 h-[50px]" : "w-full h-[40px] "}>
+      <section className={matches ? "mt-4" : "mt-10"}>
+        <form className={`flex items-center ${matches && "flex-wrap justify-between"} gap-3 w-full`}
+        >
+          <div className={!matches ? "w-2/4 h-[50px]" : "w-full h-[40px] mx-[3px]"}>
             <div className=" flex flex-col border-b border-gray-400 hover:border-transparent">
-              <label htmlFor="ipt" className="text-[13px] text-gray-500">
+              <label htmlFor="ipt" className="text-[13px] text-gray-500 ">
                 Job-Title
               </label>
               <input
@@ -79,61 +79,80 @@ const Jobs = () => {
                 className="text-base pl-2 pt-2 outline-none bg-none rounded-none border-b-2 focus:border-blue-500 border-transparent hover:border-black"
                 ref={inputRef}
                 onChange={removeFilter}
-              // onKeyDown={({ key }) => key === 'Enter' && multiQuery()}
+                // onKeyDown={({ key }) => key === 'Enter' && multiQuery()}
               />
             </div>
           </div>
-          <div className={!matches ? "w-2/4 h-[50px]" : "w-full h-[40px]"}>
-
+          <div className={!matches ? "w-2/4 h-[50px]" : "w-full h-[40px] mx-[3px]"}>
             <div className="flex flex-col border-b border-gray-400 hover:border-transparent">
-              <label htmlFor="workModelHous" className="text-[13px] text-gray-500">
+              <label
+                htmlFor="workModelHous"
+                className="text-[13px] text-gray-500"
+              >
                 Work model
               </label>
-              <select id="workModelHous" value={workModel} onChange={(event: any) => setWorkModel(event.target.value)}
+              <select
+                id="workModelHous"
+                value={workModel}
+                onChange={(event: any) => setWorkModel(event.target.value)}
                 className="outline-none bg-none rounded-none border-b-2  focus:border-blue-500 border-transparent hover:border-black pt-2 text-base"
               >
                 <option className="text-base" value={""}></option>
-                <option className="text-base" value={10}>Fulltime</option>
-                <option className="text-base" value={20}>Parttime</option>
+                <option className="text-base" value={10}>
+                  Fulltime
+                </option>
+                <option className="text-base" value={20}>
+                  Parttime
+                </option>
               </select>
             </div>
           </div>
-
-          <div className={!matches ? "w-2/4 h-[50px] " : "w-full h-[40px] "}>
+          <div className={!matches ? "w-2/4 h-[50px] " : "w-full h-[40px] mx-[3px] "}>
             <OptGruop
               value={targetCity}
               onChange={(event) => setTargetCity(event.target.value)}
             />
           </div>
           <button
-            className={` ${!matches ? "w-2/4 h-[50px]" : "w-full h-[40px] mt-2"} 
-          border-none rounded-md  bg-sky-500 text-white font-semibold hover:bg-sky-400 text-base`}
+            className={`border-none rounded-md bg-sky-500 text-white font-semibold hover:bg-sky-400 text-base 
+              ${!matches ? "w-2/4 h-[50px]" : "w-full h-[40px] mx-[3px] mt-2"}`}
             onClick={multiQuery}
             type="button"
-
           >
             Search
           </button>
         </form>
-        <div className="mt-10 w-full flex gap-2 ">
-          {queries.length === 0 
-
-          ?  (<div className="flex flex-col gap-1 min-w-full" > <CardSkeleton cards={10} /></div>)
-          :  (<ul className={`flex flex-col gap-1 ${(!openUp) ? "min-w-full" : "w-1/4"}`}>
-            {queries?.map((v) => {
-              return (
-                <li
-                  key={v.pr_jobid}
-                  onClick={() => navToJobDeatils(v.pr_jobid)}
-                  className="px-3 w-full h-16 bg-slate-100 flex items-center justify-between text-slate-700 border-2 border-solid border-slate-100 hover:border-slate-400"
-                >
-                  <div>
-                    { <p className="text-base font-semibold">{v.pr_jobtitle}</p>}
-                  </div>
-                </li>
-              );
-            })}
-          </ul>)}
+        <div className={`w-full flex gap-2 ${matches ? "mt-3" : " mt-10"}`}>
+          {queries.length === 0 ? (
+            <div className="flex flex-col gap-1 min-w-full">
+              {" "}
+              <CardSkeleton cards={10} />
+            </div>
+          ) : (
+            <ul
+              className={`flex flex-col gap-1 ${
+                !openUp ? "min-w-full" : "w-1/4"
+              }`}
+            >
+              {queries?.map((v) => {
+                return (
+                  <li
+                    key={v.pr_jobid}
+                    onClick={() => navToJobDeatils(v.pr_jobid)}
+                    className="px-3 w-full h-16 bg-slate-100 flex items-center justify-between text-slate-700 border-2 border-solid border-slate-100 hover:border-slate-400"
+                  >
+                    <div>
+                      {
+                        <p className="text-base font-semibold">
+                          {v.pr_jobtitle}
+                        </p>
+                      }
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
           {openUp && (
             <JobDetails
               Title={filtered.pr_title}
