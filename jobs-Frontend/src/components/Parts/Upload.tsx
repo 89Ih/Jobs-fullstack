21c1b1688a820f8f612e275677742cb91d95d3ea
89Ih <ehabhamza94@gmail.com>
2024-08-26 10:00:56 +0200

    responsive MUI

--- jobs-Frontend/src/components/Parts/Upload.tsx
+++ jobs-Frontend/src/components/Parts/Upload.tsx
@@ -1,16 +1,17 @@
-import { FC } from "react";
+import { ChangeEvent, FC } from "react";
 import { Icons } from "../../pages/_static";
 interface IUpload {
     label : string
     htmlFor:string
     name: string;
-    onChange: (event: any)=> void
+    onChange: (event: ChangeEvent<HTMLInputElement>)=> void
 }
 
 const Upload: FC<IUpload> = ({label,htmlFor,name,onChange}) => {
     return (
+      <div className="flex flex-col w-full">
       <div className="flex items-center gap-2 w-full">
-        <div className="shadow-gray-400 shadow-sm w-full bg-sky-100 h-20 flex flex-col justify-center items-center rounded-md focus:outline-none">
+        <div className="shadow-gray-400 shadow-sm w-full bg-sky-100 h-20 flex flex-col justify-center items-center rounded-md">
           <label
             htmlFor={htmlFor}
             className="flex flex-col items-center justify-center font-semibold text-slate-700 text-sm"
@@ -22,8 +23,9 @@ const Upload: FC<IUpload> = ({label,htmlFor,name,onChange}) => {
             />
             {label}
           </label>
-          <input type="file" name={name} id={htmlFor} onChange={onChange}  accept=".pdf, .doc, .docx"/>
+          <input className="border-none focus:border-none" type="file" name={name} id={htmlFor} onChange={onChange}  accept=".pdf, .doc, .docx"/>
         </div>
+      </div>
       </div>
     );
 }
